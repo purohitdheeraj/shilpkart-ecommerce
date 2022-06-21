97f9d7dc9d4d743b3b2065e92807af24ee486d24
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2022-06-21 17:01:36 +0530

    Task 2: Cart Checkout ✅

---
+++ src/components/products/HorizontalCard.jsx
@@ -0,0 +1,162 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { UseCartContext } from "../../context/cartContext";
+import { useWishlistContext } from "../../context/wislistContext";
+import "./productCard.css";
+const HorizontalCard = ({
+	product,
+	wishlistButtonProp,
+	cartButtonProp,
+	homeSpaProp,
+}) => {
+	const {
+		title,
+		price,
+		description,
+		imgSrc,
+		ratings,
+	} = product;
+
+	const {
+		wishlistState,
+		wishlistDispatch,
+	} = useWishlistContext();
+
+	const { cart, dispatchToCart } = UseCartContext();
+
+	const WishlistStatus = wishlistState.wishlist.find(
+		(el) => el._id === product._id
+	);
+
+	const disableWishlist = WishlistStatus ? true : false;
+
+	const CartStatus = cart.find(
+		(item) => item._id === product._id
+	);
+
+	const disableCart = CartStatus ? true : false;
+
+	return (
+		<>
+			<li className="card-horizontal card  text-center">
+				<div className="card-text">
+					<h6 className="h6">{title}</h6>
+
+					<p className="p-sm text-left">
+						{description}
+					</p>
+
+					<div className="quantity text-left">
+						<h4>Quantity:</h4>
+
+						<button>
+							+
+						</button>
+						
+						<input
+							className="input-field"
+							type="number"
+						/>
+
+						<button>
+							
+							-
+							
+						</button>
+					</div>
+
+					<div className="text-left ">
+						<h4>Price:</h4> {"  "}
+						<span className="p-sm">
+							<i className="fa fa-inr"></i>
+						</span>
+						<span className="price-tag">
+							{price.toLocaleString("en-IN")}
+							/-
+						</span>
+					</div>
+
+					<span className="text-right high">
+						{ratings}
+					</span>
+				</div>
+				<div className="card-cover badge-container">
+					{homeSpaProp ? (
+						<NavLink
+							to={`./products/${product._id}`}
+						>
+							<img src={imgSrc} alt={title} />
+						</NavLink>
+					) : (
+						<NavLink to={`./${product._id}`}>
+							<img src={imgSrc} alt={title} />
+						</NavLink>
+					)}
+
+					<div className="cart_buttons">
+						{cartButtonProp ? (
+							<button
+								className="btn btn-primary-outline"
+								onClick={() =>
+									dispatchToCart({
+										type:
+											"REMOVE_FROM_CART",
+										payload:
+											product._id,
+									})
+								}
+							>
+								Remove From Cart
+							</button>
+						) : (
+							<button
+								disabled={disableCart}
+								className="btn btn-primary"
+								onClick={() =>
+									dispatchToCart({
+										type: "ADD_TO_CART",
+										payload: product,
+									})
+								}
+							>
+								Add To Cart
+							</button>
+						)}
+
+						{wishlistButtonProp ? (
+							<button
+								className="btn btn-primary-outline"
+								onClick={() =>
+									wishlistDispatch({
+										type:
+											"REMOVE_FROM_WISHLIST",
+										payload:
+											product._id,
+									})
+								}
+							>
+								{wishlistButtonProp}
+							</button>
+						) : (
+							<button
+								className="btn btn-primary-outline"
+								disabled={disableWishlist}
+								onClick={() =>
+									wishlistDispatch({
+										type:
+											"ADD_TO_WISHLIST",
+										payload: product,
+									})
+								}
+							>
+								Add To Wishlist
+							</button>
+						)}
+					</div>
+				</div>
+			</li>
+		</>
+	);
+};
+
+export { HorizontalCard };
--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,93 +1,127 @@
 import React from "react";
-import { ProductCard } from "../../components";
+
+import { HorizontalCard } from "../../components/products/HorizontalCard";
 import { UseCartContext } from "../../context/cartContext";
 import { useDocumentTitle } from "../../utils";
 import "../Products/product.css";
-
+import "./cart.css";
+import { NavLink } from "react-router-dom";
 const Cart = () => {
 	useDocumentTitle("Cart");
 	const { cart } = UseCartContext();
-	console.log(cart);
+
 	return (
-		<div className="flex-row">
-			<article className="prd-article">
-				<ul className="card-container">
-					{cart.length > 0 ? (
-						<>
-							{cart.map((item) => (
-								<ProductCard
-									key={item._id}
-									product={item}
-									cartButtonProp="Remove From Cart"
-								/>
-							))}
-						</>
-					) : (
-						"Aiyo! Add re something to cart why empty empty scrolling our shop"
-					)}
-				</ul>
+		<main className="cart-container">
+			<article>
+				{cart.length > 0 ? (
+					<>
+						{cart.map((item) => (
+							<HorizontalCard
+								className="horizontal-card"
+								key={item._id}
+								product={item}
+								cartButtonProp="Remove From Cart"
+							/>
+						))}
+					</>
+				) : (
+					<span></span>
+				)}
 			</article>
+
 			<aside className="row cart-info">
 				<li className="card card-horizontal text-center">
 					<div className="card-text">
 						<h6 className="h6">Your Cart</h6>
 
 						<div className="products flex-column">
-							<div className="product">
-								<span className="name">
-									Shri Ganesh Idol
-								</span>
-								<span className="price">
-									40,000/-
-									<strong>X</strong>1
-								</span>
-							</div>
+							{cart.length > 0 ? (
+								<>
+									{cart.map(
+										({
+											title,
+											price,
+										}) => (
+											<div className="product">
+												<p className="name">
+													Product
+													:{" "}
+													{title}
+												</p>
+												<p className="price">
+													<b>
+														Price
+														:
+													</b>{" "}
+													{price.toLocaleString(
+														"en-IN"
+													)}
+													<strong>
+														X
+													</strong>
+													<span className="text-right">
+														1
+													</span>
+												</p>
+											</div>
+										)
+									)}
+									<div className="subtotal">
+										<small className="text-left p-md">
+											<strong>
+												Total:
+											</strong>
+											<i className="fa fa-inr"></i>
+											{cart
+												.reduce(
+													(
+														acc,
+														curr
+													) =>
+														acc +
+														curr.price,
+													0
+												)
+												.toLocaleString(
+													"en-IN"
+												)}
+										</small>
+									</div>
 
-							<div className="product">
-								<span className="name">
-									Mata Saraswati Idol
-								</span>
-								<span className="price">
-									50,000/-
-									<strong>X</strong>1
-								</span>
-							</div>
-
-							<div className="product">
-								<span className="name">
-									Shri Venkateswara
-								</span>
-								<span className="price">
-									400,000/-
-									<strong>X</strong>1
-								</span>
-							</div>
-
-							<div className="product">
-								<span>Discount (5%)</span>
-
-								<span>- 24,500/-</span>
-							</div>
+									<span
+										className="btn btn-secondary"
+										role="button"
+									>
+										Checkout
+									</span>
+								</>
+							) : (
+								<>
+									Cart is empty
+									<div className="product">
+										<h3>
+											{" "}
+											Today's Discount
+											(5%)
+										</h3>
+										<NavLink to="/products">
+											Buy Now
+										</NavLink>
+									</div>
+									<NavLink
+										to="/products"
+										className="btn btn-secondary"
+									>
+										Show me Awesome
+										Products!
+									</NavLink>
+								</>
+							)}
 						</div>
-
-						<div className="subtotal">
-							<small className="text-left p-md">
-								<strong>Sub Total:</strong>
-								<i className="fa fa-rupee"></i>
-								465,500/-
-							</small>
-						</div>
-
-						<span
-							className="btn btn-secondary"
-							role="button"
-						>
-							Checkout
-						</span>
 					</div>
 				</li>
 			</aside>
-		</div>
+		</main>
 	);
 };
 
